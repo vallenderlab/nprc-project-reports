bf194d5e96542a4e128d08ecf4674bfc504d39eb
Shaurita Hutchins <shutchins2@umc.edu>
2019-12-16 19:13:45 -0600

    Updated metadata. Added PCA.

--- R/plink_pca.R
+++ R/plink_pca.R
@@ -1,11 +1,15 @@
 library(tidyverse)
+library(gglabeller)
+library(ggrepel)
+library(ggpubr)
 
 # Import eigen vectors and values from plink
-eigenvecs <- read.table("plink_pca/all_nprcs_no_exome_pca.eigenvec")
-eigenvals <- read.table("plink_pca/all_nprcs_no_exome_pca.eigenval")
+eigenvecs <- read.table("data/plink_pca/all_nprcs_wgs_pca.eigenvec")
+eigenvals <- read.table("data/plink_pca/all_nprcs_wgs_pca.eigenval")
 
 # Import metadata
-#metadata <- read.table("metadata/all_nprcs_wgs.csv", sep = ",", header = TRUE)
+metadata <- read.table("data/metadata/all_nprcs_wgs.csv", sep = ",", header = TRUE)
+metadata <- metadata[order(metadata$mgap_id), ]
 
 # Add ids as rownames
 eigenvecs <- eigenvecs %>%
@@ -13,14 +17,53 @@ eigenvecs <- eigenvecs %>%
   column_to_rownames(var = "V1") %>%
   select(-one_of("V2"))
 
+eigenvecs <- eigenvecs[order(row.names(eigenvecs)), ]
+mutate_all(eigenvecs, funs(na_if(., "")))
+
 # Add center, origin, and gender to eigenvecs matrix columns
-ids <- sort(rownames(eigenvecs))
+eigenvecs$center <- metadata$primate_center
+eigenvecs$gender <- metadata$gender
+eigenvecs$geographic_origin <- metadata$geographic_origin
+eigenvecs$total_reads <- metadata$total_reads
 
-eigenscale <- scale(eigenvecs)
 
-p <- ggplot(data = eigenvecs, aes(x = eigenvecs$V3, y = eigenvecs$V4)) + geom_point() +
-  geom_hline(yintercept = 0, lty = 2) + geom_vline(xintercept = 0, lty = 2) + geom_point(alpha = 0.8) +
-  xlab("PC 1 (22.4%)") + ylab("PC 2 (7.24%)") + theme_minimal() + theme(
+p_go <- ggplot(data = eigenvecs, aes(x = V3, y = V4, color = geographic_origin)) + geom_point() +
+  geom_hline(yintercept = 0, lty = 2) + geom_vline(xintercept = 0, lty = 2) + geom_point(alpha = 0.8, size = 3) +
+  xlab("PC 1 (20.5%)") + ylab("PC 2 (6.2%)") + theme_minimal() + theme(
     panel.grid = element_blank(), panel.border = element_rect(fill = "transparent")
-  )
-print(p)
+  ) + guides(fill = guide_legend(title = "Legend Title", override.aes = aes(label = "")))
+
+pgo_gglab <- gglabeller(p_go, aes(label = rownames(eigenvecs)))
+
+p_go_c <- ggplot(data = eigenvecs, aes(x = V3, y = V4, color = center, shape = geographic_origin)) + geom_point() +
+  geom_hline(yintercept = 0, lty = 2) + geom_vline(xintercept = 0, lty = 2) + geom_point(alpha = 0.8, size = 3) +
+  xlab("PC 1 (20.5%)") + ylab("PC 2 (6.2%)") + theme_minimal() + theme(
+    panel.grid = element_blank(), panel.border = element_rect(fill = "transparent")
+  ) + guides(fill = guide_legend(title = "Legend Title", override.aes = aes(label = "")))
+
+pgoc_gglab <- gglabeller(p_go_c, aes(label = rownames(eigenvecs)))
+
+p_c_g <- ggplot(data = eigenvecs, aes(x = V3, y = V4, color = center, shape = gender)) + geom_point() +
+  geom_hline(yintercept = 0, lty = 2) + geom_vline(xintercept = 0, lty = 2) + geom_point(alpha = 0.8, size = 3) +
+  xlab("PC 1 (20.5%)") + ylab("PC 2 (6.2%)") + theme_minimal() + theme(
+    panel.grid = element_blank(), panel.border = element_rect(fill = "transparent")
+  ) + guides(fill = guide_legend(title = "Legend Title", override.aes = aes(label = "")))
+
+pcg_gglab <- gglabeller(p_c_g, aes(label = rownames(eigenvecs)))
+
+p_go_c_e <- p_go_c + stat_ellipse(
+  geom = "polygon", aes(fill = geographic_origin),
+
+  alpha = 0.2,
+
+  show.legend = FALSE,
+
+  level = 0.95
+)
+
+final_plot <- ggarrange(pgoc_gglab$plot, pcg_gglab$plot, ncol = 2, nrow = 1, widths = c(1.25, 1.1))
+final_plot_annotated <- annotate_figure(final_plot, 
+                                        top = text_grob("Plink Principal Components Analysis",
+                                                        size = 14))
+
+ggsave("data/plink_pca/plink_pca.png", plot = final_plot_annotated, dpi = 450, width = 16, height = 9)
--- R/truffle.R
+++ R/truffle.R
@@ -1,6 +1,9 @@
 library(gplots)
 library(RColorBrewer)
 library(cluster)
+library(truffle)
+
+data <- truffle::import_truffle(path = "data/truffle_runs/all_nprcs_rminfo_auto_wgs_biall_chrUn.ibd")
 
 snpgdsTRUFFLE2GDS <- function() {
 
