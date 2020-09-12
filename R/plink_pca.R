bf5697f17c34aa8a3338a10314266d02bd098663
Shaurita Hutchins <shaurita.d.hutchins@gmail.com>
2020-09-12 10:27:28 -0500

    Added data fles.

--- R/plink_pca.R
+++ R/plink_pca.R
@@ -6,8 +6,8 @@ library(ggrepel)
 library(ggpubr)
 
 # Import eigen vectors and values from plink
-eigenvecs <- read.table("data/plink_pca/all_nprcs_wgs_pca.eigenvec")
-eigenvals <- read.table("data/plink_pca/all_nprcs_wgs_pca.eigenval")
+eigenvecs <- read.table("data/plink_pca/all_auto_wgs_all_biallelic_maf05.eigenvec")
+eigenvals <- read.table("data/plink_pca/all_auto_wgs_all_biallelic_maf05.eigenval")
 
 # Import metadata
 metadata <- read.table("data/metadata/all_nprcs_wgs.csv", sep = ",", header = TRUE)
@@ -31,7 +31,7 @@ eigenvecs$total_reads <- metadata$total_reads
 
 p_go <- ggplot(data = eigenvecs, aes(x = V3, y = V4, color = geographic_origin)) + geom_point() +
   geom_hline(yintercept = 0, lty = 2) + geom_vline(xintercept = 0, lty = 2) + geom_point(alpha = 0.8, size = 3) +
-  xlab("PC 1 (20.5%)") + ylab("PC 2 (6.2%)") + theme_minimal() + theme(
+  xlab("PC 1 (12.7%)") + ylab("PC 2 (5.82%)") + theme_minimal() + theme(
     panel.grid = element_blank(), panel.border = element_rect(fill = "transparent")
   ) + guides(fill = guide_legend(title = "Legend Title", override.aes = aes(label = "")))
 
@@ -39,7 +39,7 @@ pgo_gglab <- gglabeller(p_go, aes(label = rownames(eigenvecs)))
 
 p_go_c <- ggplot(data = eigenvecs, aes(x = V3, y = V4, color = center, shape = geographic_origin)) + geom_point() +
   geom_hline(yintercept = 0, lty = 2) + geom_vline(xintercept = 0, lty = 2) + geom_point(alpha = 0.8, size = 3) +
-  xlab("PC 1 (20.5%)") + ylab("PC 2 (6.2%)") + theme_minimal() + theme(
+  xlab("PC 1 (12.7%)") + ylab("PC 2 (5.82%)") + theme_minimal() + theme(
     panel.grid = element_blank(), panel.border = element_rect(fill = "transparent")
   ) + guides(fill = guide_legend(title = "Legend Title", override.aes = aes(label = "")))
 
@@ -47,13 +47,13 @@ pgoc_gglab <- gglabeller(p_go_c, aes(label = rownames(eigenvecs)))
 
 p_c_g <- ggplot(data = eigenvecs, aes(x = V3, y = V4, color = center, shape = gender)) + geom_point() +
   geom_hline(yintercept = 0, lty = 2) + geom_vline(xintercept = 0, lty = 2) + geom_point(alpha = 0.8, size = 3) +
-  xlab("PC 1 (20.5%)") + ylab("PC 2 (6.2%)") + theme_minimal() + theme(
+  xlab("PC 1 (12.7%)") + ylab("PC 2 (5.82%)") + theme_minimal() + theme(
     panel.grid = element_blank(), panel.border = element_rect(fill = "transparent")
   ) + guides(fill = guide_legend(title = "Legend Title", override.aes = aes(label = "")))
 
 pcg_gglab <- gglabeller(p_c_g, aes(label = rownames(eigenvecs)))
 
-p_go_c_e <- p_go_c + stat_ellipse(
+p_go_c_e <- pgoc_gglab$plot + stat_ellipse(
   geom = "polygon", aes(fill = geographic_origin),
 
   alpha = 0.2,
@@ -68,4 +68,4 @@ final_plot_annotated <- annotate_figure(final_plot,
                                         top = text_grob("Plink Principal Components Analysis",
                                                         size = 14))
 
-ggsave("data/plink_pca/plink_pca.png", plot = final_plot_annotated, dpi = 450, width = 16, height = 9)
+ggsave("data/plink_pca/plink_pca_all_auto_wgs_all_biallelic_maf05_ellipses.png", plot = final_plot_annotated, dpi = 450, width = 16, height = 9)
